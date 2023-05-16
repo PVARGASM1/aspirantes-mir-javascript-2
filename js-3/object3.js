beedb6db745c7f65699aeb03c52df440462d0171
Andrea <andrea.vargas18@hotmail.es>
2023-05-16 15:27:50 -0500

    solucion a los ejercicios de js

--- js-3/object3.js
+++ js-3/object3.js
@@ -4,16 +4,19 @@ receta.nombre = "Sandwich"
 
 receta.ingredientes = []
 
-receta.ingredientes.uno = {
+receta.ingredientes.push({
     nombre: "Pan",
-    cantidad: 2
-}
-receta.ingredientes.dos ={
+    cantidad: 2})
+
+receta.ingredientes.push({
     nombre: "Queso",
-    cantidad: 1
+    cantidad: 1})
+
+console.log(receta.ingredientes[0].nombre)
+
+let sumaCantidad = 0
+
+for (let i =0; i<receta.ingredientes.length; i++){
+  sumaCantidad +=receta.ingredientes[i].cantidad;
 }
-console.log(receta.ingredientes.uno.nombre)
-
-let sumaCantidad = receta.ingredientes.uno.cantidad + receta.ingredientes.dos.cantidad
-
-console.log(sumaCantidad)
+console.log(`La suma de las cantidades es ${sumaCantidad}`)
